68098ea3ad7d1c19503625342bb80b2b09081660
Muhammad Faridz <faridzmuhamad679@gmail.com>
2026-01-04 07:09:50 +0700

    update

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,6 +2,7 @@ import { createAuthClient } from "better-auth/react"
 import { usernameClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
+    baseURL: import.meta.env.VITE_BASE_URL,
     plugins: [ 
         usernameClient() 
     ],
